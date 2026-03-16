45b3faa9910f4fdc6cd46c168b22ebbd133a926c
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 23:38:37 +0100

    feat: add implementation for the basic host

--- model/http/HostModel.py
+++ model/http/HostModel.py
@@ -2,5 +2,19 @@ from abc import ABC, abstractmethod
 
 
 class AbstractHost(ABC):
-    _my_endpoint: str
+    _my_url: str
     _my_port: int
+
+    def __init__(self, url: str, port: int):
+        self._my_url = url
+        self._my_port = port
+
+    def get_url(self) -> str:
+        return self._my_url
+
+    def get_port(self) -> int:
+        return self._my_port
+
+class BasicHost(AbstractHost):
+    def __init__(self, url: str, port: int):
+        super().__init__(url, port)
