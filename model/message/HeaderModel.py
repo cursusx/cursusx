e67c8b771359d97789c7e3128a4e4956929f67ed
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 22:23:09 +0100

    feat: add getter for the header

--- model/message/HeaderModel.py
+++ model/message/HeaderModel.py
@@ -14,12 +14,18 @@ class AbstractHeader(ABC):
         self._my_header_name = my_header_name
         self._my_header_value = my_header_value
 
+    def get_header_name(self) -> str:
+        return self._my_header_name
+
+    def get_header_value(self) -> str:
+        return self._my_header_value
+
 class Header(AbstractHeader):
     def __init__(self, my_header_name: str, my_header_value: str):
         super().__init__(my_header_name, my_header_value)
 
     @classmethod
-    def from_tuple(cls, value: tuple[str, str]=('', '')) -> 'AbstractHeader':
+    def from_tuple(cls, value: tuple[str, str] = ('', '')) -> 'AbstractHeader':
         if not value or len(value) != 2:
             raise ValueError('The input value must not be empty and not None.')
         return cls(value[0], value[1])
