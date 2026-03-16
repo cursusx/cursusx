a645a8094d67ec14582cf395bae6e52ed8d31920
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 23:15:58 +0100

    test: assert it raises an exception when the input collection is empty

--- model/test/method/TestHeadersModel.py
+++ model/test/method/TestHeadersModel.py
@@ -19,3 +19,7 @@ def test_should_have_duplicate_headers() -> None:
     }
 
     assert all(header in expected for header in headers.get_headers())
+
+def test_should_raise_error_when_input_collection_is_empty() -> None:
+    with pytest.raises(ValueError):
+        headers: Headers = Headers.from_list(headers=[])
