e37c4d5371b68d64e1cb7cc88934783436a3324b
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 22:19:40 +0100

    test: should be possible to create a header from a tuple

--- model/test/message/test_header_message.py
+++ model/test/message/test_header_message.py
@@ -3,7 +3,13 @@ import pytest
 from model.message.HeaderModel import Header
 
 def test_should_raise_value_error_when_value_is_empty() -> None:
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         Header.from_tuple()
+
+def test_should_be_possible_to_create_header_from_non_empty_tuple() -> None:
+    try:
+        Header.from_tuple(('content-type', 'text/html'))
+    except ValueError:
+        pytest.fail('When the input is a tuple it should be ok.')
 
 
