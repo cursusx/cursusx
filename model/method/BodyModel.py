d69cc0355ad46b7032df639517b7b8c97172fda5
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 23:24:22 +0100

    feat: add Body Implementation

--- model/method/BodyModel.py
+++ model/method/BodyModel.py
@@ -1,8 +1,30 @@
 from abc import ABC
 
+_SENTINEL = object()
 
 class AbstractBody(ABC):
+    """
+    This class represents the method's body.
+    """
     _my_content: str
 
     def __init__(self, my_content: str=''):
         self._my_content = my_content
+
+    def get_content(self) -> str:
+        return self._my_content
+
+class Body(AbstractBody):
+    def __init__(self, _sentinel: object=None, *, content: str):
+        if _sentinel is not _SENTINEL:
+            raise ValueError('In order to create the body use the factory method.')
+        super().__init__(content)
+
+    @classmethod
+    def from_string(cls, content: str='') -> 'Body':
+        """
+        This method creates a Body from the input content string.
+        :param content: input content to store
+        :return: a new Body object
+        """
+        return cls(_sentinel=_SENTINEL, content=content)
