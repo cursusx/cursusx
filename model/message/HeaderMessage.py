a3160151ae385ca6ce7d791a9ac8ad90fb104eab
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 17:34:02 +0100

    feat: add factory method for header message

--- model/message/HeaderMessage.py
+++ model/message/HeaderMessage.py
@@ -8,6 +8,14 @@ class AbstractHeaderMessage(ABC):
     my_header_name: str
     my_header_value: str
 
+    @classmethod
+    def from_tuple(cls, value: tuple[str, str]=None) -> 'AbstractHeaderMessage':
+        if not value:
+            raise ValueError('The input value must not be empty and not None.')
+        return cls(value[0], value[1])
+
+
+
 @dataclass(frozen=True)
 class Headers(NamedTuple):
     my_headers: List[AbstractHeaderMessage] = field(default_factory=list)
