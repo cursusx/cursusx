9483d093467448aefeced3c85896945ddd9b082c
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 17:15:53 +0100

    feat: add partial implementation of the constructor

--- model/message/HeaderMessage.py
+++ model/message/HeaderMessage.py
@@ -1,9 +1,15 @@
 from abc import ABC
+from typing import NamedTuple
 
 
 class AbstractHeaderMessage(ABC):
     my_header_name: str
     my_header_value: str
 
-class Headers:
+class Headers(NamedTuple):
     my_headers: list[AbstractHeaderMessage]
+
+    def __new__(cls, my_headers: list[AbstractHeaderMessage], *, _internal: bool=False):
+        if not _internal:
+            raise TypeError('You can not use the default contructor, instead use the factory class method.')
+        cls.my_headers = my_headers
