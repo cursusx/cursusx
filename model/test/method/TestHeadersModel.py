c6d635dfb4fd5e19ebb489736cf0f226143da381
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 23:13:26 +0100

    test: assert header does not contain duplicates

--- model/test/method/TestHeadersModel.py
+++ model/test/method/TestHeadersModel.py
@@ -1,7 +1,21 @@
+from typing import Set
+
 import pytest
 
-from model.method.HeaderModel import Headers
+from model.method.HeaderModel import Headers, Header, AbstractHeader
 
 
-def test_should_avoid_to_have_none_values() -> None:
-    pass
+def test_should_have_duplicate_headers() -> None:
+    headers: Headers = Headers.from_list([
+        Header.from_tuple(('a', 'b')),
+        Header.from_tuple(('a', 'b')),
+        Header.from_tuple(('c', 'd'))
+    ])
+    assert len(headers.get_headers()) == 2
+
+    expected: Set[AbstractHeader] = {
+        Header.from_tuple(('a', 'b')),
+        Header.from_tuple(('c', 'd'))
+    }
+
+    assert all(header in expected for header in headers.get_headers())
