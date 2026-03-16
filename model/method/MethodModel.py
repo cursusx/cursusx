4a9b9bde2f68fec0c618ff25516084c20d9ff783
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 22:44:57 +0100

    feat: add request and response implementation

--- model/method/MethodModel.py
+++ model/method/MethodModel.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from dataclasses import dataclass
 from http import HTTPStatus
 
 from model.method.HeaderModel import Headers
@@ -21,3 +22,17 @@ class AbstractMethod(ABC):
 
     def get_headers(self) -> Headers:
         return self._my_headers
+
+class ResponseMethod(AbstractMethod):
+    """
+    This class represents a Response http method.
+    """
+    def __init__(self, status_code: HTTPStatus, headers: Headers):
+        super().__init__(status_code, headers)
+
+class RequestMethod(AbstractMethod):
+    """
+    This class represents a Request http method.
+    """
+    def __init__(self, status_code: HTTPStatus, headers: Headers):
+        super().__init__(status_code, headers)
