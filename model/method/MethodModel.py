f25b03edf892724a382e7afb8ed47af597541ea7
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 23:19:40 +0100

    feat: add body implementation

--- model/method/MethodModel.py
+++ model/method/MethodModel.py
@@ -2,6 +2,7 @@ from abc import ABC
 from dataclasses import dataclass
 from http import HTTPStatus
 
+from model.method.BodyModel import AbstractBody
 from model.method.HeaderModel import Headers
 
 
@@ -12,10 +13,12 @@ class AbstractMethod(ABC):
     """
     _my_status_code: HTTPStatus
     _my_headers: Headers
+    _my_body: AbstractBody
 
-    def __init__(self, status_code: HTTPStatus, headers: Headers) -> None:
+    def __init__(self, status_code: HTTPStatus, headers: Headers, body: AbstractBody) -> None:
         self._my_status_code = status_code
         self._my_headers = headers
+        self._my_body = body
 
     def get_status_code(self) -> HTTPStatus:
         """
@@ -31,16 +34,23 @@ class AbstractMethod(ABC):
         """
         return self._my_headers
 
+    def get_body(self) -> AbstractBody:
+        """
+        This method returns the body of the http method.
+        :return: see above
+        """
+        return self._my_body
+
 class ResponseMethod(AbstractMethod):
     """
     This class represents a Response http method.
     """
-    def __init__(self, status_code: HTTPStatus, headers: Headers):
-        super().__init__(status_code, headers)
+    def __init__(self, status_code: HTTPStatus, headers: Headers, body: AbstractBody) -> None:
+        super().__init__(status_code, headers, body)
 
 class RequestMethod(AbstractMethod):
     """
     This class represents a Request http method.
     """
-    def __init__(self, status_code: HTTPStatus, headers: Headers):
-        super().__init__(status_code, headers)
+    def __init__(self, status_code: HTTPStatus, headers: Headers, body: AbstractBody) -> None:
+        super().__init__(status_code, headers, body)
