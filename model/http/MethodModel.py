266d4c7857c4d30a7fade1a68563a71706e19f96
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 00:12:38 +0100

    feat: add http method and http request

--- model/http/MethodModel.py
+++ model/http/MethodModel.py
@@ -1,9 +1,18 @@
 from abc import ABC, abstractmethod
+from http import HTTPMethod
 
 from model.http.ContentModel import RequestContent, ResponseContent
 
+# TODO: add tests
+
 
 class AbstractHttpMethod(ABC):
-    @abstractmethod
-    def do_method(self, request: RequestContent) -> ResponseContent:
-        pass
+    _my_http_method: HTTPMethod
+    _my_http_request: RequestContent
+
+    def __init__(self, method: HTTPMethod, request: RequestContent):
+        self._my_http_method = method
+        self._my_http_request = request
+
+    def get_http_request(self) -> RequestContent:
+        return self._my_http_request
