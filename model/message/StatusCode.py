2c34a57628b7b841fe3a197f0869d6d4ee294c8d
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-16 17:01:39 +0100

    feat: create status code factory

--- model/message/StatusCode.py
+++ model/message/StatusCode.py
@@ -6,8 +6,14 @@ class StatusCode(NamedTuple):
     my_status_code: int
     my_status_message: str
 
+    def __new__(cls, message: str, status_code: int, *, _internal=False):
+        if not _internal:
+            raise TypeError('You cannot create a StatusCode object, instead use the factory.')
+        cls.my_status_code = status_code
+        cls.my_status_message = message
+
     @classmethod
     def from_status_code(cls, status_code: int) -> 'StatusCode':
         if all_my_status_codes.get(status_code):
-            return cls(status_code, all_my_status_codes[status_code])
+            return cls(all_my_status_codes[status_code], status_code, _internal=True)
         raise ValueError(f"The following status code {status_code} is not supported or likely wrong.")
