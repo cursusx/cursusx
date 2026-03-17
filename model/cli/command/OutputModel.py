15c6cd9a7df9b7fee9716e8e5747639ca676e8a2
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 12:45:15 +0100

    feat: wrap output with generics

--- model/cli/command/OutputModel.py
+++ model/cli/command/OutputModel.py
@@ -1,8 +1,15 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from typing_extensions import TypeVar, Generic
+
+_T = TypeVar("_T")
+_O = TypeVar("_O")
 
 
-class AbstractOutput(ABC):
+class AbstractOutput(ABC, Generic[_T, _O]):
     """
     Wrapper for the output.
     """
-    pass
+    @abstractmethod
+    def wrap_output(self, output: _T) -> _O:
+        pass
