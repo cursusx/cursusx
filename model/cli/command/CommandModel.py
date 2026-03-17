4e634d2d1c82adb0c7b09810cc78ad5a3d675407
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 13:07:58 +0100

    refactor: move the http command line into another package
    refactor: move

--- model/cli/command/CommandModel.py
+++ model/cli/command/CommandModel.py
@@ -1,10 +1,6 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 from model.cli.command.FlagModel import AbstractFlag
-from model.cli.command.OutputModel import AbstractOutput, HttpOutput
-from model.http.engine.HttpEngineModel import AbstractHttpEngine
-from model.http.info.ContentModel import ResponseContent
-from model.http.info.MethodModel import AbstractHttpMethod
 
 
 class AbstractCommand(ABC):
@@ -24,27 +20,3 @@ class AbstractCommand(ABC):
         for flag in self._my_flags:
             output += f"{flag.get_flag_descritpion()}\n"
         return output
-
-
-class AbstractHttpCommand(AbstractCommand):
-    _my_http_engine: AbstractHttpEngine
-
-    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: set[AbstractFlag]):
-        super().__init__(command_name, flags)
-        self._my_http_engine = http_engine
-
-    @abstractmethod
-    def execute_http_command(self, request: AbstractHttpMethod) -> AbstractOutput:
-        pass
-
-
-class HttpCommand(AbstractHttpCommand):
-    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: set[AbstractFlag]):
-        super().__init__(http_engine, command_name, flags)
-
-    def get_description(self) -> str:
-        return f"Command name: {self._my_command_name}. \n" + super().get_description()
-
-    def execute_http_command(self, request: AbstractHttpMethod) -> AbstractOutput:
-        response: ResponseContent = self._my_http_engine.do_query(request)
-        return HttpOutput(response)
---
+++ model/cli/command/http/HttpCommandModel.py
@@ -0,0 +1,32 @@
+from abc import abstractmethod
+
+from model.cli.command.CommandModel import AbstractCommand
+from model.cli.command.FlagModel import AbstractFlag
+from model.cli.command.OutputModel import AbstractOutput, HttpOutput
+from model.http.engine.HttpEngineModel import AbstractHttpEngine
+from model.http.info.ContentModel import ResponseContent
+from model.http.info.MethodModel import AbstractHttpMethod
+
+
+class AbstractHttpCommand(AbstractCommand):
+    _my_http_engine: AbstractHttpEngine
+
+    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: set[AbstractFlag]):
+        super().__init__(command_name, flags)
+        self._my_http_engine = http_engine
+
+    @abstractmethod
+    def execute_http_command(self, request: AbstractHttpMethod) -> AbstractOutput:
+        pass
+
+
+class HttpCommand(AbstractHttpCommand):
+    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: set[AbstractFlag]):
+        super().__init__(http_engine, command_name, flags)
+
+    def get_description(self) -> str:
+        return f"Command name: {self._my_command_name}. \n" + super().get_description()
+
+    def execute_http_command(self, request: AbstractHttpMethod) -> AbstractOutput:
+        response: ResponseContent = self._my_http_engine.do_query(request)
+        return HttpOutput(response)
