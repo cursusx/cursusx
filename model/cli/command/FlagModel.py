42db0040287cc863faf8ea2c5db091cbf653f021
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 13:44:48 +0100

    feat: add implementation for the Flag Value

--- model/cli/command/FlagModel.py
+++ model/cli/command/FlagModel.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
+from re import Pattern
+
+from typing_extensions import TypeVar
 
 from model.cli.command.CommandModel import AbstractCommand
 from model.cli.command.OutputModel import AbstractOutput
+
+_V = TypeVar("_V")
 
 
 class AbstractFlagLogic(ABC):
@@ -10,9 +15,26 @@ class AbstractFlagLogic(ABC):
         pass
 
 
+class AbstractFlagValue(ABC):
+    _my_representation: Pattern
+
+    def is_valid_flag_value(self, flag_value: str) -> bool:
+        """
+        This method allows to check if the flag is valid.
+        :param flag_value: input string that represent the flag value
+        :return: see above
+        """
+        return self._my_representation.match(flag_value) is not None
+
+
 class AbstractFlag(ABC):
+    """
+    This class represents a specific command flag, defined in this way:
+    -name=value
+    """
     _my_name: str
     _my_logic: AbstractFlagLogic
+    _my_flag_values: AbstractFlagValue
 
     def __init__(self, my_name: str, my_logic: AbstractFlagLogic):
         self._my_name = my_name
