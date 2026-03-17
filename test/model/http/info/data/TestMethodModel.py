d1d1862bd4c2c567fd2b74c34e6def45f1515710
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 15:27:04 +0100

    test: assert that is possible to correctly create a http data

--- test/model/http/info/data/TestMethodModel.py
+++ test/model/http/info/data/TestMethodModel.py
@@ -3,8 +3,9 @@ from http import HTTPMethod, HTTPStatus
 from model.http.info.body.BodyModel import Body
 from model.http.info.content.RequestModel import RequestContent, RequestContentBuilder
 from model.http.info.data.DataModel import AbstractHttpData, HttpData
-from model.http.info.header.HeaderModel import Headers, Header
-from model.http.info.parameter.ParameterModel import Parameters, Parameter
+from model.http.info.endpoint.EndpointModel import BasicEndpoint
+from model.http.info.header.HeaderModel import Headers, Header, AbstractHeader
+from model.http.info.parameter.ParameterModel import Parameters, Parameter, AbstractParameter
 
 
 def test_should_be_possible_to_retrieve_data_attributes():
@@ -17,9 +18,20 @@ def test_should_be_possible_to_retrieve_data_attributes():
         ]))
         .add_parameters(Parameters.from_list([Parameter.from_key_value('key', 'value')]))
         .add_status_code(HTTPStatus.OK)
+        .add_endpoint(BasicEndpoint.create_endpoint('localhost', 80))
         .build()
     )
     data: AbstractHttpData = HttpData.create_data(method,
                                                   request)
+    expected_parameters: set[AbstractParameter] = {
+        Parameter.from_key_value('key', 'value')}
+    expected_headers: set[AbstractHeader] = {
+        Header.from_tuple(('key', 'value'))}
     assert data.get_http_method() == method
     assert data.get_http_request() is not None
+    assert all(param in expected_parameters for param in data.get_http_request(
+    ).get_parameters().get_parameters())
+    assert all(header in expected_headers for header in data.get_http_request(
+    ).get_headers().get_headers())
+    assert data.get_http_request().get_status_code() == HTTPStatus.OK
+    assert data.get_http_request().get_endpoint().dump() == "localhost:80"
