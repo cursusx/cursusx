bf9ee6ba79b0a8a929a6dadbac032d6c338171b2
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 12:43:31 +0100

    feat: add basic implementation for abstract command class

--- model/cli/command/CommandModel.py
+++ model/cli/command/CommandModel.py
@@ -1,11 +1,19 @@
 from abc import ABC, abstractmethod
 
+from model.cli.command.FlagModel import AbstractFlag
 from model.cli.command.OutputModel import AbstractOutput
 from model.http.engine.HttpEngineModel import AbstractHttpEngine
 from model.http.info.MethodModel import AbstractHttpMethod
 
 
 class AbstractCommand(ABC):
+    _my_command_name: str
+    _my_flags: dict[str, AbstractFlag]
+
+    def __init__(self, command_name: str, flags: dict[str, AbstractFlag]):
+        self._my_command_name = command_name
+        self._my_flags = dict(flags)
+
     @abstractmethod
     def get_description(self) -> str:
         """
@@ -17,6 +25,10 @@ class AbstractCommand(ABC):
 
 class AbstractHttpCommand(AbstractCommand):
     _my_http_engine: AbstractHttpEngine
+
+    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: dict[str, AbstractFlag]):
+        super().__init__(command_name, flags)
+        self._my_http_engine = http_engine
 
     @abstractmethod
     def execute_http_command(self, request: AbstractHttpMethod) -> AbstractOutput:
