248d10047f334080ea4378056ba5cc367cf7388d
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 11:34:19 +0100

    feat: add parameters inside the content class

--- model/http/info/ContentModel.py
+++ model/http/info/ContentModel.py
@@ -4,6 +4,7 @@ from http import HTTPStatus
 from model.http.info.EndpointModel import AbstractEndpoint
 from model.http.info.BodyModel import AbstractBody
 from model.http.info.HeaderModel import Headers
+from model.http.info.ParameterModel import Parameters
 
 # TODO: ADD TESTS
 _SENTINEL = object()
@@ -17,12 +18,14 @@ class AbstractContent(ABC):
     _my_endpoint: AbstractEndpoint
     _my_status_code: HTTPStatus
     _my_headers: Headers
+    _my_parameters: Parameters
     _my_body: AbstractBody
 
-    def __init__(self, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, body: AbstractBody) -> None:
+    def __init__(self, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, parameters: Parameters, body: AbstractBody) -> None:
         self._my_endpoint = endpoint
         self._my_status_code = status_code
         self._my_headers = headers
+        self._my_parameters = parameters
         self._my_body = body
 
     def get_endpoint(self) -> AbstractEndpoint:
@@ -46,6 +49,9 @@ class AbstractContent(ABC):
         """
         return self._my_headers
 
+    def get_parameters(self) -> Parameters:
+        return self._my_parameters
+
     def get_body(self) -> AbstractBody:
         """
         This http returns the body of the http http.
@@ -59,18 +65,19 @@ class ResponseContent(AbstractContent):
     This class represents a http Response.
     """
 
-    def __init__(self, _sentinel: object = None, *, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, body: AbstractBody) -> None:
+    def __init__(self, _sentinel: object = None, *, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, parameters: Parameters, body: AbstractBody) -> None:
         if _sentinel is _SENTINEL:
             raise TypeError(
                 "In order to create a Response you have to use the factory http.")
-        super().__init__(endpoint, status_code, headers, body)
+        super().__init__(endpoint, status_code, headers, parameters, body)
 
     @classmethod
-    def create_response(cls, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, body: AbstractBody) -> 'ResponseContent':
+    def create_response(cls, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, parameters: Parameters, body: AbstractBody) -> 'ResponseContent':
         return cls(_sentinel=_SENTINEL,
                    endpoint=endpoint,
                    status_code=status_code,
                    headers=headers,
+                   parameters=parameters,
                    body=body)
 
 
@@ -79,13 +86,12 @@ class RequestContent(AbstractContent):
     This class represents a http Request.
     """
 
-    def __init__(self, _sentinel: object = None, *, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, body: AbstractBody) -> None:
+    def __init__(self, _sentinel: object = None, *, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, parameters: Parameters, body: AbstractBody) -> None:
         if _sentinel is _SENTINEL:
             raise TypeError(
                 "In order to create a Request you have to use the factory http.")
-        super().__init__(endpoint, status_code, headers, body)
+        super().__init__(endpoint, status_code, headers, parameters, body)
 
     @classmethod
-    def create_request(cls, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers,
-                       body: AbstractBody) -> 'RequestContent':
-        return cls(_sentinel=_SENTINEL, endpoint=endpoint, status_code=status_code, headers=headers, body=body)
+    def create_request(cls, endpoint: AbstractEndpoint, status_code: HTTPStatus, headers: Headers, parameters: Parameters, body: AbstractBody) -> 'RequestContent':
+        return cls(_sentinel=_SENTINEL, endpoint=endpoint, status_code=status_code, headers=headers, parameters=parameters, body=body)
