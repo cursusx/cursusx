585ed214bc073223bfe9cd528ea6cfd335353a59
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 11:52:35 +0100

    feat: add implementation for generic execute query method

--- model/http/engine/StandardEngineModel.py
+++ model/http/engine/StandardEngineModel.py
@@ -1,10 +1,37 @@
+from http import HTTPStatus
+from typing import Iterable, Mapping, Any
+
 import requests
+from requests import Response
 
 from model.http.engine.EngineModel import AbstractEngine
+from model.http.info.BodyModel import Body
 from model.http.info.ContentModel import ResponseContent
+from model.http.info.EndpointModel import AbstractEndpoint
+from model.http.info.HeaderModel import Headers
 from model.http.info.MethodModel import AbstractHttpMethod
+from model.http.info.ParameterModel import Parameters
 
 
-class StandardEngine:
+class StandardEngine(AbstractEngine):
     def __init__(self):
-        super().__init__()
+        super().__init__('requests')
+
+    def _do_get(self, http_request: AbstractHttpMethod) -> ResponseContent:
+        return self._execute_request(http_request.get_http_method(),
+                                     http_request)
+
+    def _execute_request(self, method: str, request: AbstractHttpMethod) -> ResponseContent:
+        response: Response = requests.request(method=method,
+                                              url=request.get_http_request().get_endpoint().dump(),
+                                              params=request.get_http_request().get_parameters().dump(),
+                                              headers=request.get_http_request().get_headers().dump(),
+                                              data=request.get_http_request().get_body().get_content())
+        return ResponseContent.create_response(endpoint=request.get_http_request().get_endpoint(),
+                                               status_code=HTTPStatus(
+                                                   response.status_code),
+                                               headers=Headers.from_dictionary(
+                                                   response.headers),
+                                               parameters=Parameters.from_list(
+                                                   []),
+                                               body=Body.from_string(response.text))
