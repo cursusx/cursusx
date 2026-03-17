577a7342dd6b415e3dd6c26b5bcb03969dade777
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 13:06:05 +0100

    feat: improve the command description by printing all the flahs

--- model/cli/command/CommandModel.py
+++ model/cli/command/CommandModel.py
@@ -9,25 +9,27 @@ from model.http.info.MethodModel import AbstractHttpMethod
 
 class AbstractCommand(ABC):
     _my_command_name: str
-    _my_flags: dict[str, AbstractFlag]
+    _my_flags: set[AbstractFlag]
 
-    def __init__(self, command_name: str, flags: dict[str, AbstractFlag]):
+    def __init__(self, command_name: str, flags: set[AbstractFlag]):
         self._my_command_name = command_name
-        self._my_flags = dict(flags)
+        self._my_flags = set(flags)
 
-    @abstractmethod
     def get_description(self) -> str:
         """
         This method returns the description of the command.
         :return:
         """
-        pass
+        output: str = "The following command supports all these flags:"
+        for flag in self._my_flags:
+            output += f"{flag.get_flag_descritpion()}\n"
+        return output
 
 
 class AbstractHttpCommand(AbstractCommand):
     _my_http_engine: AbstractHttpEngine
 
-    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: dict[str, AbstractFlag]):
+    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: set[AbstractFlag]):
         super().__init__(command_name, flags)
         self._my_http_engine = http_engine
 
@@ -37,14 +39,11 @@ class AbstractHttpCommand(AbstractCommand):
 
 
 class HttpCommand(AbstractHttpCommand):
-    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: dict[str, AbstractFlag]):
+    def __init__(self, http_engine: AbstractHttpEngine, command_name: str, flags: set[AbstractFlag]):
         super().__init__(http_engine, command_name, flags)
 
     def get_description(self) -> str:
-        # TODO: ADD flag dump
-        return f"""
-        This command executes http commands, all the supported flags are:
-        """
+        return f"Command name: {self._my_command_name}. \n" + super().get_description()
 
     def execute_http_command(self, request: AbstractHttpMethod) -> AbstractOutput:
         response: ResponseContent = self._my_http_engine.do_query(request)
