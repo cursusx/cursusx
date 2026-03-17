5995a3e4fe3e2e7a14bcf2424f7d30884f0060ac
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-17 10:35:02 +0100

    test: assert body content is kept

--- test/model/http/info/TestBodyModel.py
+++ test/model/http/info/TestBodyModel.py
@@ -6,3 +6,9 @@ from model.http.info.BodyModel import AbstractBody, Body
 def test_should_be_possible_to_create_empty_body():
     body: AbstractBody = Body.from_string()
     assert len(body.get_content()) == 0
+
+
+def test_body_should_be_possible_to_retrieve_it():
+    body: AbstractBody = Body.from_string('Random Body')
+    assert body.get_content() is not None
+    assert body.get_content() == 'Random Body'
