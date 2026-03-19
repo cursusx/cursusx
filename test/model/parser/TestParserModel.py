e3949b6471a6f555ae025f7df17b6a6024309972
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-19 14:28:56 +0100

    test: create mock server in the setup and kill it in the tear down

--- test/model/parser/TestParserModel.py
+++ test/model/parser/TestParserModel.py
@@ -1,10 +1,15 @@
 from model.parser.ParserModel import AbstractParser, Parser
 from test.mock.ServerMock import start_mock_server, kill_mock_server
+from test.util.BaseServerTestCase import BaseServerTestCase
 
 
-def test_should_be_possible_to_run_http_command():
-    server = start_mock_server('localhost', 8080)
-    command: str = "http -method=get -endpoint=localhost:8080"
-    parser: AbstractParser = Parser()
-    parser.execute_command(command)
-    kill_mock_server(server)
+class ParseTest(BaseServerTestCase):
+    __test__ = True
+
+    def __init__(self):
+        super().__init__('localhost', 8080)
+
+    def test_should_be_possible_to_run_http_command(self) -> None:
+        command: str = "http -method=get -endpoint=localhost:8080"
+        parser: AbstractParser = Parser()
+        parser.execute_command(command)
