a6f936fbf370c555694ffcca60cbc67751b85832
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-19 13:11:17 +0100

    test: specify endpoint for the http mock server

--- test/model/parser/TestParserModel.py
+++ test/model/parser/TestParserModel.py
@@ -3,8 +3,8 @@ from test.mock.ServerMock import start_mock_server
 
 
 def test_should_be_possible_to_run_http_command():
-    server = start_mock_server('localhost', 7000)
-    command: str = "http -method=get"
+    server = start_mock_server('localhost', 8080)
+    command: str = "http -method=get -endpoint=localhost:8080"
     parser: AbstractParser = Parser()
     parser.execute_command(command)
     server.server_close()
