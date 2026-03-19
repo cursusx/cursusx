ed1f3d824ab4abef13bc950c9ac428f12b824725
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-19 13:11:05 +0100

    refactor: use factory method

--- model/cli/command/http/flags/HttpEndpointFlagModel.py
+++ model/cli/command/http/flags/HttpEndpointFlagModel.py
@@ -7,7 +7,7 @@ from model.http.info.endpoint.EndpointModel import BasicEndpoint
 class HttpEndpointFlagValue(AbstractFlagValue[HttpDataBuilder]):
     def match_value(self) -> None:
         self._my_query_builder.add_endpoint(
-            BasicEndpoint(url='localhost', port=8000))
+            BasicEndpoint.create_endpoint(url='localhost', port=8000))
 
 
 class HttpEndpointFlag(AbstractFlag[HttpDataBuilder]):
