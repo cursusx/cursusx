367fc68e9baffc4b373fd96550f4f19eb79e2fb5
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-19 12:58:26 +0100

    feat: create abstract class for generic mock handler

--- test/mock/ServerMock.py
+++ test/mock/ServerMock.py
@@ -1,15 +1,43 @@
+from abc import abstractmethod, ABC
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-class BasicMockHandler(BaseHTTPRequestHandler):
+class AbstractMockHandler(ABC, BaseHTTPRequestHandler):
+    @abstractmethod
+    def do_GET(self) -> None:
+        pass
+
+    @abstractmethod
+    def do_POST(self) -> None:
+        pass
+
+    @abstractmethod
+    def do_PUT(self) -> None:
+        pass
+
+    @abstractmethod
+    def do_DELETE(self) -> None:
+        pass
+
+
+class BasicMockHandler(AbstractMockHandler):
     def do_DELETE(self) -> None:
         self.send_response(200)
+        self.end_headers()
 
     def do_PUT(self) -> None:
         self.send_response(200)
+        self.end_headers()
 
     def do_POST(self) -> None:
         self.send_response(200)
+        self.end_headers()
 
     def do_GET(self) -> None:
         self.send_response(200)
+        self.end_headers()
+
+
+def create_mock_server(url: str, port: int, mock_handler: type[AbstractMockHandler]) -> HTTPServer:
+    # type: ignore[arg-type]
+    return HTTPServer(server_address=(url, port), RequestHandlerClass=mock_handler)
