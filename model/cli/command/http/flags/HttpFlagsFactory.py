a32f083e7d06b34c680f10f88bd95acc134e8422
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-19 13:09:38 +0100

    feat: add new implementation for factory endpoint flag class

--- model/cli/command/http/flags/HttpFlagsFactory.py
+++ model/cli/command/http/flags/HttpFlagsFactory.py
@@ -1,9 +1,11 @@
 from model.cli.command.FlagFactory import AbstractFlagFactory
-from model.cli.command.http.Contants import HTTP_FLAG_METHOD
+from model.cli.command.http.Contants import HTTP_FLAG_METHOD, HTTP_FLAG_ENDPOINT
+from model.cli.command.http.flags.HttpEndpointFlagModel import HttpEndpointFlag, HttpEndpointFlagValue
 from model.cli.command.http.flags.HttpMethodFlagModel import HttpMethodFlag, HttpMethodFlagValue
 
 
 class HttpFlagFactory(AbstractFlagFactory):
     _my_flags = {
         HTTP_FLAG_METHOD: (HttpMethodFlag, HttpMethodFlagValue),
+        HTTP_FLAG_ENDPOINT: (HttpEndpointFlag, HttpEndpointFlagValue),
     }
