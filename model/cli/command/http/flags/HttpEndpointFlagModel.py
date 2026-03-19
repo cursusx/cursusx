61e5c71aa268375fd901d6eebd9a05321bf9de33
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-19 14:36:16 +0100

    feat: use hardcoded endpoint for testing purposes

--- model/cli/command/http/flags/HttpEndpointFlagModel.py
+++ model/cli/command/http/flags/HttpEndpointFlagModel.py
@@ -7,7 +7,7 @@ from model.http.info.endpoint.EndpointModel import BasicEndpoint
 class HttpEndpointFlagValue(AbstractFlagValue[HttpDataBuilder]):
     def match_value(self) -> None:
         self._my_query_builder.add_endpoint(
-            BasicEndpoint.create_endpoint(url='localhost', port=8000))
+            BasicEndpoint.create_endpoint(url='http://localhost', port=8080))
 
 
 class HttpEndpointFlag(AbstractFlag[HttpDataBuilder]):
