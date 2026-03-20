fe1cf967545faafd7af420a881867245dae6fce0
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-20 10:59:25 +0100

    feat: add implementation for headers flag value class

--- model/cli/command/http/flags/HttpHeadersFlagModel.py
+++ model/cli/command/http/flags/HttpHeadersFlagModel.py
@@ -1,5 +1,16 @@
 from model.cli.command.FlagModel import AbstractFlag, AbstractFlagValue
 from model.http.info.data.DataModel import HttpDataBuilder
+from model.http.info.header.HeaderModel import Headers
+
+
+class HttpHeadersFlagValue(AbstractFlagValue[HttpDataBuilder]):
+    def __init__(self, my_query_builder: HttpDataBuilder, flag_value: str):
+        super().__init__(my_query_builder, flag_value)
+
+    def match_value(self) -> None:
+        # TODO: Add factory from string.
+        self._my_query_builder.add_headers(
+            Headers.from_dictionary({}))
 
 
 class HttpHeadersFlag(AbstractFlag[HttpDataBuilder]):
