4a3f998f5532dfac159f937d26c9cfcb1e190a06
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 15:05:48 +0100

    feat: create factory method for command wrapper

---
+++ wrapper/ViewCommandFactory.py
@@ -0,0 +1,10 @@
+from model.cli.command.OutputModel import AbstractOutput
+from model.cli.command.http.HttpOutputModel import HttpOutput
+from view.CommandWidget import CommandWidget
+from wrapper.http.HttpCommandWrapper import HttpCommandWrapper
+
+
+def get_viewer_command_widget[T](output: AbstractOutput[T]) -> CommandWidget:
+    if isinstance(output, HttpOutput):
+        return HttpCommandWrapper(output)
+    raise NotImplementedError("The output format is not yet supported")
