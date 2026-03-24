5b2dfd39f1afcd8624f908928d9cd33844d09431
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 15:08:47 +0100

    refactor: remove the print commands

--- cursusx.py
+++ cursusx.py
@@ -14,7 +14,6 @@ special_pre_command: set[str] = {PRETTY_OUTPUT}
 def main():
     start_command_index: int = 2 if sys.argv[1] in special_pre_command else 1
     command: str = ' '.join(sys.argv[start_command_index:])
-    print(command)
     output: AbstractOutput = ParserFactory[str]().from_command(
         input_command=command).execute_command(command)
     if sys.argv[1] == PRETTY_OUTPUT:
