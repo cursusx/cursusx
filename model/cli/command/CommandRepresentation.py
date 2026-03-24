e0a97277e55c7c98be75476b4b6bd151bf866ff0
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 11:07:44 +0100

    refactor: adjust the input strategy for parsing the command, use new regex and avoid to split on spaces

--- model/cli/command/CommandRepresentation.py
+++ model/cli/command/CommandRepresentation.py
@@ -28,7 +28,8 @@ class AbstractExtractCommandFlagStrategy(ABC, Generic[_I, _T]):
 
 
 class StringCommandFlagStrategy(AbstractExtractCommandFlagStrategy[str, _T]):
-    _my_flag_with_value_regex: Pattern[str] = re.compile(r'-(\w+)(?:=(\S+))?')
+    _my_flag_with_value_regex: Pattern[str] = re.compile(
+        r'-(\w+)(?:=(.+?))?(?=\s+-\w|$)', re.DOTALL)
 
     def __init__(self, my_query_data_builder: AbstractBuilder[_T],
                  flag_factory: AbstractFlagFactory):
@@ -36,13 +37,12 @@ class StringCommandFlagStrategy(AbstractExtractCommandFlagStrategy[str, _T]):
 
     def extract_flag_representation(self, input_command: str,
                                     ) -> set[AbstractFlag]:
-        array_of_flags: list[str] = input_command.split()[1:]
+        rest: str = input_command.split(
+            None, 1)[1] if ' ' in input_command else ''
         all_flags: set[AbstractFlag] = set()
-        for single_flag in array_of_flags:
-            # a flag is of the type: -flag_name?(=value)
-            matches: Match[str] | None = self._my_flag_with_value_regex.match(
-                single_flag)
+        for matches in self._my_flag_with_value_regex.finditer(rest):
             if matches:
+                print(matches)
                 flag_value: str = matches.group(2) if matches.group(2) else ''
                 all_flags.add(self._my_flag_factory.create_flag(
                     matches.group(1), flag_value, self._my_query_data_builder))
