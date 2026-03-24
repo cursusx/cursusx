c47c323fe4e6640f4aaa1e0e7032f55c5f002d66
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 16:27:09 +0100

    feat: update the style of the css based on the status code

--- wrapper/http/StatusCodeWrapper.py
+++ wrapper/http/StatusCodeWrapper.py
@@ -1,34 +1,45 @@
+from http import HTTPStatus
+
 from view.item.LabelItem import LabelItem
 
 
 class StatusCodeWrapper(LabelItem):
     DEFAULT_CSS = """
-           StatusCodeWrapper {
-               height: auto;
-               margin: 1 0;
-               border: round $primary;
-               border-title-align: left;
-               padding: 1 2;
-           }
+               StatusCodeWrapper {
+                   height: auto;
+                   margin: 1 0;
+                   border-title-align: left;
+                   padding: 1 2;
+               }
 
-           StatusCodeWrapper .label {
-               color: $text-muted;
-           }
+               StatusCodeWrapper .label {
+                   text-style: bold; 
+                   color: $text-muted;
+               }
 
-           StatusCodeWrapper .value {
-               color: $text;
-           }
-           
-           StatusCodeWrapper .success {
-               color: round green;
-           }
-           
-           StatusCodeWrapper .error{
-               color: round red;
-           }
-           """
+               StatusCodeWrapper .value {
+                   color: $text;
+               }
 
-    def __init__(self, value: str, **kwargs):
-        super().__init__(label="Status Code", value=value, **kwargs)
+               StatusCodeWrapper.success {
+                   border: round green;
+               }
+
+               StatusCodeWrapper.success .label {
+                   color: green;
+               }
+
+               StatusCodeWrapper.error {
+                   border: round red;
+               }
+
+               StatusCodeWrapper.error .label {
+                   color: red;
+               }
+               """
+
+    def __init__(self, value: HTTPStatus, **kwargs):
+        super().__init__(label="Status Code",
+                         value=f"Code:{value.value} → {value.name}", **kwargs)
         self.remove_class("success", "error")
-        self.add_class("success" if int(value) < 400 else "error")
+        self.add_class("success" if value.value < 400 else "error")
