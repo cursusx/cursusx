e6d5326f7ba19b1a262cd8030aa3def468148367
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 16:19:01 +0100

    feat: create Status Code Wrapper

--- wrapper/http/StatusCodeWrapper.py
+++ wrapper/http/StatusCodeWrapper.py
@@ -2,5 +2,33 @@ from view.item.LabelItem import LabelItem
 
 
 class StatusCodeWrapper(LabelItem):
+    DEFAULT_CSS = """
+           StatusCodeWrapper {
+               height: auto;
+               margin: 1 0;
+               border: round $primary;
+               border-title-align: left;
+               padding: 1 2;
+           }
+
+           StatusCodeWrapper .label {
+               color: $text-muted;
+           }
+
+           StatusCodeWrapper .value {
+               color: $text;
+           }
+           
+           StatusCodeWrapper .success {
+               color: round green;
+           }
+           
+           StatusCodeWrapper .error{
+               color: round red;
+           }
+           """
+
     def __init__(self, value: str, **kwargs):
         super().__init__(label="Status Code", value=value, **kwargs)
+        self.remove_class("success", "error")
+        self.add_class("success" if int(value) < 400 else "error")
