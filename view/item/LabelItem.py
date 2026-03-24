8132c631fb87fad612438036d8246888d79867d8
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 14:50:29 +0100

    refactor: add label nme and label value to display

--- view/item/LabelItem.py
+++ view/item/LabelItem.py
@@ -1,6 +1,18 @@
+from textual.app import ComposeResult
+from textual.containers import Vertical
 from textual.widgets import Static
 
 
-class LabelItem(Static):
-    def __init__(self, value: str, **kwargs):
+class LabelItem(Vertical, Static):
+
+    _my_label: str
+    _my_value: str
+
+    def __init__(self, label: str, value: str, **kwargs):
         super().__init__(value, **kwargs)
+        self._my_label = label
+        self._my_value = value
+
+    def compose(self) -> ComposeResult:
+        yield Static(self._my_label, classes="label")
+        yield Static(self._my_value, classes="value")
