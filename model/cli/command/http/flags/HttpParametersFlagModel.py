72490e77587e22edd152188efc9954256a14b6e2
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 11:24:34 +0100

    feat: init new class HttpParametersFlagValue

--- model/cli/command/http/flags/HttpParametersFlagModel.py
+++ model/cli/command/http/flags/HttpParametersFlagModel.py
@@ -1,7 +1,18 @@
-from model.cli.command.FlagModel import AbstractFlag
+from model.cli.command.FlagModel import AbstractFlag, AbstractFlagValue
 from model.http.info.data.DataModel import HttpDataBuilder
 
 
+class HttpParametersFlagValue(AbstractFlagValue[HttpDataBuilder]):
+    def __init__(self, my_query_builder: HttpDataBuilder, flag_value: str):
+        super().__init__(my_query_builder, flag_value)
+
+    def match_value(self) -> None:
+        pass
+
+
 class HttpParametersFlag(AbstractFlag[HttpDataBuilder]):
+    def __init__(self, my_flag_value: AbstractFlagValue[HttpDataBuilder]):
+        super().__init__(my_flag_value)
+
     def get_flag_descritpion(self) -> str:
         return "This flag allows to specify the query parameters."
