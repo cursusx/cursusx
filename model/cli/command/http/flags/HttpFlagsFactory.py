6cb231721d0fa1d61b1cb47530eac294302b4a44
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 10:51:55 +0100

    feat: add factory object for parsing headers flag

--- model/cli/command/http/flags/HttpFlagsFactory.py
+++ model/cli/command/http/flags/HttpFlagsFactory.py
@@ -1,6 +1,7 @@
 from model.cli.command.FlagFactory import AbstractFlagFactory
-from model.cli.command.http.Contants import HTTP_FLAG_METHOD, HTTP_FLAG_ENDPOINT
+from model.cli.command.http.Contants import HTTP_FLAG_METHOD, HTTP_FLAG_ENDPOINT, HTTP_FLAG_HEADERS
 from model.cli.command.http.flags.HttpEndpointFlagModel import HttpEndpointFlag, HttpEndpointFlagValue
+from model.cli.command.http.flags.HttpHeadersFlagModel import HttpHeadersFlag, HttpHeadersFlagValue
 from model.cli.command.http.flags.HttpMethodFlagModel import HttpMethodFlag, HttpMethodFlagValue
 
 
@@ -8,4 +9,5 @@ class HttpFlagFactory(AbstractFlagFactory):
     _my_flags = {
         HTTP_FLAG_METHOD: (HttpMethodFlag, HttpMethodFlagValue),
         HTTP_FLAG_ENDPOINT: (HttpEndpointFlag, HttpEndpointFlagValue),
+        HTTP_FLAG_HEADERS: (HttpHeadersFlag, HttpHeadersFlagValue),
     }
