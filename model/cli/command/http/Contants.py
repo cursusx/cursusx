8b91b6f186e33f6810044333ad6cf8f5dd6a76bc
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 11:17:49 +0100

    feat: add parameters flag constant

--- model/cli/command/http/Contants.py
+++ model/cli/command/http/Contants.py
@@ -3,6 +3,7 @@ STANDARD_ENGINE_NAME: str = 'requests'
 HTTP_FLAG_METHOD: str = "method"
 HTTP_FLAG_ENDPOINT: str = "endpoint"
 HTTP_FLAG_HEADERS: str = "headers"
+HTTP_FLAG_PARAMETERS: str = "parameters"
 
 # HTTP COMMAND NAME
 HTTP_COMMAND_NAME = "http"
