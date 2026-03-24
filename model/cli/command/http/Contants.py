c361cdc61f3541f77cb80e792384030897317a5c
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 10:50:42 +0100

    feat: add new constant representing the headers flag

--- model/cli/command/http/Contants.py
+++ model/cli/command/http/Contants.py
@@ -2,6 +2,7 @@ STANDARD_ENGINE_NAME: str = 'requests'
 # FLAGS
 HTTP_FLAG_METHOD: str = "method"
 HTTP_FLAG_ENDPOINT: str = "endpoint"
+HTTP_FLAG_HEADERS: str = "headers"
 
 # HTTP COMMAND NAME
 HTTP_COMMAND_NAME = "http"
