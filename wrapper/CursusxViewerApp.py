263a90fe907c7d288034fd38f03ebe6f2ffad99e
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 14:35:30 +0100

    refactor: use the generic command widget for wrapping the output style

--- wrapper/CursusxViewerApp.py
+++ wrapper/CursusxViewerApp.py
@@ -1,27 +1,14 @@
 from textual.app import App, ComposeResult
 
-from view.item.DictionaryViewerItem import DictionaryViewerItem
-from view.item.LabelItem import LabelItem
-from view.item.StringViewerItem import StringViewerItem
+from view.CommandWidget import CommandWidget
 
 
 class CursusxViewerApp(App):
-    _my_dictionary_items: list[DictionaryViewerItem]
-    _my_label_items: list[LabelItem]
-    _my_string_items: list[StringViewerItem]
+    _my_command_widget: CommandWidget
 
-    def __init__(self, dictionary_items: list[DictionaryViewerItem],
-                 label_items: list[LabelItem],
-                 string_viewer_items: list[StringViewerItem], **kwargs):
+    def __init__(self, command_app: CommandWidget, **kwargs):
         super().__init__(**kwargs)
-        self._my_dictionary_items = dictionary_items
-        self._my_label_items = label_items
-        self._my_string_items = string_viewer_items
+        self._my_command_widget = command_app
 
     def compose(self) -> ComposeResult:
-        for item in self._my_dictionary_items:
-            yield item
-        for item in self._my_label_items:
-            yield item
-        for item in self._my_string_items:
-            yield item
+        yield self._my_command_widget
