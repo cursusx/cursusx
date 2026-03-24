8da1750eb5a61b3c66cc2524bf8c12792be2c12f
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 15:50:36 +0100

    feat: add advanced check for input endpoint
    refactor
    
    feat: register on "Q" event the kill of the entire application
    
    feat: feat

--- model/cli/command/http/flags/HttpEndpointFlagModel.py
+++ model/cli/command/http/flags/HttpEndpointFlagModel.py
@@ -7,8 +7,7 @@ from model.http.info.data.DataModel import HttpDataBuilder
 from model.http.info.endpoint.EndpointModel import BasicEndpoint
 
 IPV4 = r'(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)'
-IPV6 = r'(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}'          # full form
-# compressed (::1, etc.)
+IPV6 = r'(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}'
 IPV6_COMPRESSED = r'(?:[0-9a-fA-F]{0,4}:){2,7}[0-9a-fA-F]{0,4}'
 HOST = rf'(?:{IPV4}|{IPV6}|\[{IPV6_COMPRESSED}\]|localhost)'
 PORT = r'(?:[1-9]\d{0,4}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])'
@@ -18,16 +17,28 @@ class HttpEndpointFlagValue(AbstractFlagValue[HttpDataBuilder]):
     # ip:port
     # http://ip:port
 
-    _my_pattern: Pattern[str] = re.compile(
+    _my_pattern_with_protocol: Pattern[str] = re.compile(
+        rf'^(https?://)({HOST}):({PORT})$')
+
+    _my_pattern_without_protocol: Pattern[str] = re.compile(
         rf'^(?:https?://)?({HOST}):({PORT})$')
 
     def match_value(self) -> None:
-        matches: Match | None = self._my_pattern.match(self._my_flag_value)
-        if matches is None:
-            raise Exception(
-                f"Invalid http endpoint value: {self._my_flag_value}, you should specify the ip and the port and the protocol.")
-        self._my_query_builder.add_endpoint(
-            BasicEndpoint.create_endpoint(url=matches.group(1), port=int(matches.group(2))))
+        matches: Match | None = self._my_pattern_with_protocol.match(
+            self._my_flag_value)
+        if matches:
+            self._my_query_builder.add_endpoint(
+                BasicEndpoint.create_endpoint(url=f"{matches.group(1)}{matches.group(2)}", port=int(matches.group(3))))
+        else:
+            matches: Match | None = self._my_pattern_without_protocol.match(
+                self._my_flag_value)
+            if matches:
+                self._my_query_builder.add_endpoint(
+                    BasicEndpoint.create_endpoint(url=matches.group(1),
+                                                  port=int(matches.group(2))))
+            else:
+                raise Exception(
+                    f"Invalid http endpoint value: {self._my_flag_value}, you should specify the ip and the port and the protocol.")
 
 
 class HttpEndpointFlag(AbstractFlag[HttpDataBuilder]):
--- wrapper/CursusxViewerApp.py
+++ wrapper/CursusxViewerApp.py
@@ -1,3 +1,6 @@
+from typing import Coroutine, Any
+
+from textual import events
 from textual.app import App, ComposeResult
 
 from view.CommandWidget import CommandWidget
@@ -9,6 +12,10 @@ class CursusxViewerApp(App):
     def __init__(self, command_app: CommandWidget, **kwargs):
         super().__init__(**kwargs)
         self._my_command_widget = command_app
+
+    async def _on_key(self, event: events.Key) -> None:
+        if event.key == "Q":
+            self.exit()
 
     def compose(self) -> ComposeResult:
         yield self._my_command_widget
