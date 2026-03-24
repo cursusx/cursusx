fffbd2b48ceb4b5cf249a37f9be7315c831d6afb
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 11:49:54 +0100

    refactor(test): rename a test scenario

--- test/model/http/info/parameter/TestParametersModel.py
+++ test/model/http/info/parameter/TestParametersModel.py
@@ -20,8 +20,3 @@ def test_should_have_duplicate_parameters() -> None:
 
     assert all(
         parameters in expected for parameters in parameters.get_parameters_set())
-
-
-def test_should_raise_error_when_input_collection_is_empty() -> None:
-    with pytest.raises(ValueError):
-        params: Parameters = Parameters.from_list(parameters=[])
