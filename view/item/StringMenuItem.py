b4dd9412e85fb4db3f2bddc434b16c971bbacb6e
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-24 12:30:00 +0100

    refactor: create specific file

--- view/item/StringMenuItem.py
+++
@@ -1,6 +0,0 @@
-
-class StringMenuItem:
-    _my_item_name: str
-
-    def __init__(self, item_name: str):
-        self._my_item_name = item_name
---
+++ view/item/http/HeadersItem.py
@@ -0,0 +1,9 @@
+from textual.widgets import Collapsible
+
+
+class HeadersItem(Collapsible):
+    DEFAULT_CSS = """
+    HeadersItem: {
+        margin: 1 2;
+    }
+    """
