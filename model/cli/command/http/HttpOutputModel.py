c0ed1b029ceaaa6ade6dc4169ded9edd41ac3f82
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-26 11:42:36 +0100

    feat: add type
    f

--- model/cli/command/http/HttpOutputModel.py
+++ model/cli/command/http/HttpOutputModel.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from model.cli.command.OutputModel import AbstractOutput
 from model.http.info.content.ResponseModel import ResponseContent
 
@@ -7,10 +9,9 @@ class HttpOutput(AbstractOutput[ResponseContent]):
         super().__init__(output)
 
     def wrap_output(self) -> str:
-        res = self.get_output()
-        status_code = res.get_status_code()
+        res: ResponseContent = self.get_output()
+        status_code: HTTPStatus = res.get_status_code()
 
-        # Building the string with a structured, boxed aesthetic
         output = "╭" + "─" * 60 + "╮\n"
         output += f"│ ⚡ HTTP RESPONSE: {status_code} ({status_code.name})\n"
         output += "├" + "─" * 60 + "┤\n"
@@ -20,7 +21,7 @@ class HttpOutput(AbstractOutput[ResponseContent]):
         output += "├" + "─" * 60 + "┤\n"
 
         output += "│ 📄 BODY\n"
-        body_content = res.get_body().get_content()
+        body_content: str = res.get_body().get_content()
         indented_body = "\n".join(
             [f"│ {line}" for line in body_content.splitlines()])
         output += f"{indented_body}\n"
