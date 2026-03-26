cecf953af96e17cd1f6f824350c77908e102351d
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-26 12:16:29 +0100

    test: use socket gethostbyname method for querying localhost ip

--- tests/model/parser/TestParserModel.py
+++ tests/model/parser/TestParserModel.py
@@ -1,3 +1,4 @@
+import socket
 from http import HTTPStatus
 from typing import cast
 
@@ -10,10 +11,11 @@ class ParseTest(BaseServerTestCase):
     __test__ = True
 
     def __init__(self, method_name: str = "runTest"):
-        super().__init__('http://localhost', 8080, method_name)
+        url: str = socket.gethostbyname('localhost')
+        super().__init__(url, 8080, method_name)
 
     def test_should_be_possible_to_run_http_command(self) -> None:
-        command: str = "http -method=get -endpoint=http://localhost:8080"
+        command: str = f"http -method=get -endpoint=http://{socket.gethostbyname('localhost')}:8080"
         output: HttpOutput = ParserFactory[str]().from_command(
             input_command=command).execute_command(command)
         assert output.get_output().get_status_code() == HTTPStatus.OK
