b10515b120abbaf7e2798fcd2453e6b31aab036f
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-26 14:30:44 +0100

    feat: add implementation for http cookies flag value

--- model/cli/command/http/flags/HttpCookiesFlagModel.py
+++ model/cli/command/http/flags/HttpCookiesFlagModel.py
@@ -1,5 +1,15 @@
 from model.cli.command.FlagModel import AbstractFlag, AbstractFlagValue
+from model.http.info.cookie.CookieModel import Cookies
 from model.http.info.data.DataModel import HttpDataBuilder
+
+
+class HttpCookiesFlagValue(AbstractFlagValue[HttpDataBuilder]):
+    def __init__(self, my_query_builder: HttpDataBuilder, flag_value: str):
+        super().__init__(my_query_builder, flag_value)
+
+    def match_value(self) -> None:
+        self._my_query_builder.add_cookies(
+            Cookies.from_string(self._my_flag_value))
 
 
 class HttpCookiesFlag(AbstractFlag[HttpDataBuilder]):
